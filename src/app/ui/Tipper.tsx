b3a0b942f2100e125ba5a157724a743f5a25245d
vkpatva <vkpatva.it@gmail.com>
2024-08-30 23:09:15 +0530

    ui-fix

--- src/app/ui/Tipper.tsx
+++ src/app/ui/Tipper.tsx
@@ -39,7 +39,7 @@ const Tipper = () => {
           <div className="flex flex-col md:flex-row items-center bg-gray-50 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 min-h-[300px]">
             <a href="#" className="w-full md:w-auto  lg:min-w-[300px]">
               <img
-                className="object-cover w-full h-full rounded-lg sm:rounded-none sm:rounded-l-lg"
+                className="object-cover w-full h-48 md:h-full rounded-t-lg md:rounded-none md:rounded-l-lg"
                 src="https://cdn.gencraft.com/prod/user/e3b1d056-a391-4365-acd5-45c7d2f69279/6746e3b7-e720-4840-95e1-7a0713608e4d/image/image1_0.jpg?Expires=1725121477&Signature=biuMolLE0CDRrl-U8isU4ejTiwHhjLD-a84lTOjRoaZKyEIhO0D~fp4KWTreuz-6r20ivVCqYXYqmphajboSNWWEQodIzvZ7wlRnEhutmsF90RsY9WqkMpyjztfQ-2ah8p7s0Vc19vTW3ruoyQ53JvjQjrw1zbuFVyO4SGVjdXtWkUQ-Nbz0h0SlIZLlafLbztm6-P4YXcdpdIpstKMIJMvICHzAtOrnZrpaiB6IRhC-MK~ntPjq0hM60U2OHfwHvC2Cgx7hKktHQVMucXOuu5czE5~XDa9ji0UCcp8zNOETns0JneQfkjKXQkoQBOxKE5DOqttkqSNYrdddhUi4eA__&Key-Pair-Id=K3RDDB1TZ8BHT8"
                 alt="Priya Sharma"
               />
@@ -62,7 +62,7 @@ const Tipper = () => {
           <div className="flex flex-col md:flex-row items-center bg-gray-50 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 min-h-[300px]">
             <a href="#" className="w-full md:w-auto  lg:min-w-[300px]">
               <img
-                className="object-cover w-full h-full rounded-lg sm:rounded-none sm:rounded-l-lg"
+                className="object-cover w-full h-48 md:h-full rounded-t-lg md:rounded-none md:rounded-l-lg"
                 src="https://cdn.gencraft.com/prod/user/e3b1d056-a391-4365-acd5-45c7d2f69279/6746e3b7-e720-4840-95e1-7a0713608e4d/image/image0_0.jpg?Expires=1725121477&Signature=WYzhuYvTylFl4v033DUZkGJCOD4qe791J3VPQnZw4TxQJbAZksCa5kOjVqiSxDaopPa9ClbyKbbRpuveF7sc8Vug3MsioMA0H8P3lie-NYx4JW-iZgI3cpS9BD0FQX20fzrTWcM3HpdScYsDCnk5GjJQtu8hLUV6p0WbIBwGRi5zxA7MWDJXdNx8beddHEzkNaV1KbyGbgWPTXglwI4z6b-TIXGaoL8c7Pre6Eol31pSxyfjfCyuPuLxU0I~2wmZLUjefcDe6xB5ndaoV6~aHgibxHq2jsp0hnFg~kCUaOqBBrwWmG83ZZEYv6fr3m2xut73UTC9A6xaElGmBasnqw__&Key-Pair-Id=K3RDDB1TZ8BHT8"
                 alt="Neha Reddy"
               />
@@ -85,7 +85,7 @@ const Tipper = () => {
           <div className="flex flex-col md:flex-row items-center bg-gray-50 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 min-h-[300px]">
             <a href="#" className="w-full md:w-auto  lg:min-w-[300px]">
               <img
-                className="object-cover w-full h-full rounded-lg sm:rounded-none sm:rounded-l-lg"
+                className="object-cover w-full h-48 md:h-full rounded-t-lg md:rounded-none md:rounded-l-lg"
                 src="https://cdn.gencraft.com/prod/user/e3b1d056-a391-4365-acd5-45c7d2f69279/c1ce15c9-1a9d-4dce-9007-240d189e5a6f/image/image0_0.jpg?Expires=1725121402&Signature=bxRbyhcx8m3lQipDiJi7mX2MD6GRx0dCvd2m5ClARkbVJZxyNjuIyazhU7Wy3b28loBV24gKxrFsgEBBk4tnHhL6CZU425cYnjubayp7o2YE1IJykx5dKS79V1f2tAzdLGE-dS8pK6Q~5X8MBI-KE6F1BWCvcAynKlgWchkFkZP3wdmHrShOUPN0aFdIOhD38u9e-2m17OCvSgxwEpAea19S2gQIh3KRkg324jo0bGgrXgb4LYknmM2XTHHaCtN-iXa-2xQwo5sZcFS2ExHRKyprc~Wwzk526NrI51LT8BI0fk9B0-04gjBnYN2Ba0xa9ZxwxU6p5KHETFFypIgACg__&Key-Pair-Id=K3RDDB1TZ8BHT8"
                 alt="Amit Patel"
               />
@@ -117,50 +117,3 @@ const Tipper = () => {
 };
 
 export default Tipper;
-
-// <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
-//   <div className="flex items-center bg-gray-50 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 min-h-[300px]">
-//     <a href="#" className="h-full min-w-[240px]">
-//       <img
-//         className="object-cover w-full h-full rounded-lg sm:rounded-none sm:rounded-l-lg"
-//         src="https://cdn.gencraft.com/prod/user/e3b1d056-a391-4365-acd5-45c7d2f69279/c1ce15c9-1a9d-4dce-9007-240d189e5a6f/image/image1_0.jpg?Expires=1725121402&Signature=JI5S0V9ZZHrzsaRUGVwHxn7gWNk7-SMEMirEKnK46xLlPXuqxK3z64NNlQ9lca2lEVRVb4DuQxGN0EH5gUzxmI50AjTfXYaMseNLKOqlMVxuKQro0bCqZjC4ArtOdfbkn3gJgpsMB4gZjjb7VHZPwTaWnLs08Ra6fLdBvXjJw-ext2mjmChCjNHexgjOIftA4A5UyuptKQsBFXY6bQIxSqZ1Rsq1NZAwecMzvMu3pJmRS2gI-QQzQ1A-fhgcaGYoIDAtyd5kRrw6UPFcTuEvTIirMI367xQ~ubp1jQggOcMnBqXBuPK0EvZZvzSG8var6vwzytvYdUKqoYgn6Xyclg__&Key-Pair-Id=K3RDDB1TZ8BHT8"
-//         alt="Rajesh Kumar"
-//       />
-//     </a>
-//     <div className="px-5">
-//       <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-//         <a href="#">Rajesh Kumar</a>
-//       </h3>
-//       <p className="font-light text-gray-500 dark:text-gray-400 mb-4 min-h-[180px]">
-//         Rajesh is a skilled waiter at Swaad Restaurant, celebrated for his quick
-//         service and friendly demeanor. His attention to detail ensures that
-//         every guest feels welcomed and well-cared for.
-//       </p>
-//       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-//         Tip Rajesh
-//       </button>
-//     </div>
-//   </div>
-//   <div className="flex items-center bg-gray-50 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 min-h-[300px]">
-//     <a href="#" className="h-full min-w-[240px]">
-//       <img
-//         className="object-cover w-full h-full rounded-lg sm:rounded-none sm:rounded-l-lg"
-//         src="https://cdn.gencraft.com/prod/user/e3b1d056-a391-4365-acd5-45c7d2f69279/6746e3b7-e720-4840-95e1-7a0713608e4d/image/image1_0.jpg?Expires=1725121477&Signature=biuMolLE0CDRrl-U8isU4ejTiwHhjLD-a84lTOjRoaZKyEIhO0D~fp4KWTreuz-6r20ivVCqYXYqmphajboSNWWEQodIzvZ7wlRnEhutmsF90RsY9WqkMpyjztfQ-2ah8p7s0Vc19vTW3ruoyQ53JvjQjrw1zbuFVyO4SGVjdXtWkUQ-Nbz0h0SlIZLlafLbztm6-P4YXcdpdIpstKMIJMvICHzAtOrnZrpaiB6IRhC-MK~ntPjq0hM60U2OHfwHvC2Cgx7hKktHQVMucXOuu5czE5~XDa9ji0UCcp8zNOETns0JneQfkjKXQkoQBOxKE5DOqttkqSNYrdddhUi4eA__&Key-Pair-Id=K3RDDB1TZ8BHT8"
-//         alt="Priya Sharma"
-//       />
-//     </a>
-//     <div className="px-5">
-//       <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-//         <a href="#">Priya Sharma</a>
-//       </h3>
-//       <p className="font-light text-gray-500 dark:text-gray-400 mb-4 min-h-[180px]">
-//         Priya is a dedicated server at Swaad Restaurant, known for her
-//         exceptional customer service and warm smile. She always goes the extra
-//         mile to ensure a pleasant dining experience for all patrons.
-//       </p>
-//       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-//         Tip Priya
-//       </button>
-//     </div>
-//   </div>
-// </div>;
