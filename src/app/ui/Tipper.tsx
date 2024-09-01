93117ec18328ab1fdbd2a500ae84ac92440d8f76
vkpatva <vkpatva.it@gmail.com>
2024-09-01 10:58:04 +0530

    fix: image fix

--- src/app/ui/Tipper.tsx
+++ src/app/ui/Tipper.tsx
@@ -19,7 +19,7 @@ const Tipper = () => {
             <a href="#" className="w-full md:w-auto lg:min-w-[300px]">
               <img
                 className="object-cover w-full h-full rounded-lg sm:rounded-none sm:rounded-l-lg"
-                src="https://cdn.gencraft.com/prod/user/e3b1d056-a391-4365-acd5-45c7d2f69279/c1ce15c9-1a9d-4dce-9007-240d189e5a6f/image/image1_0.jpg?Expires=1725121402&Signature=JI5S0V9ZZHrzsaRUGVwHxn7gWNk7-SMEMirEKnK46xLlPXuqxK3z64NNlQ9lca2lEVRVb4DuQxGN0EH5gUzxmI50AjTfXYaMseNLKOqlMVxuKQro0bCqZjC4ArtOdfbkn3gJgpsMB4gZjjb7VHZPwTaWnLs08Ra6fLdBvXjJw-ext2mjmChCjNHexgjOIftA4A5UyuptKQsBFXY6bQIxSqZ1Rsq1NZAwecMzvMu3pJmRS2gI-QQzQ1A-fhgcaGYoIDAtyd5kRrw6UPFcTuEvTIirMI367xQ~ubp1jQggOcMnBqXBuPK0EvZZvzSG8var6vwzytvYdUKqoYgn6Xyclg__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+                src="/CaZ6ZjKWqCN2UasNrrbTonogwEGvttrBWqKwgKWXgnqG.jpg"
                 alt="Rajesh Kumar"
               />
             </a>
@@ -57,7 +57,7 @@ const Tipper = () => {
             <a href="#" className="w-full md:w-auto  lg:min-w-[300px]">
               <img
                 className="object-cover w-full h-full rounded-lg sm:rounded-none sm:rounded-l-lg"
-                src="https://cdn.gencraft.com/prod/user/e3b1d056-a391-4365-acd5-45c7d2f69279/6746e3b7-e720-4840-95e1-7a0713608e4d/image/image1_0.jpg?Expires=1725121477&Signature=biuMolLE0CDRrl-U8isU4ejTiwHhjLD-a84lTOjRoaZKyEIhO0D~fp4KWTreuz-6r20ivVCqYXYqmphajboSNWWEQodIzvZ7wlRnEhutmsF90RsY9WqkMpyjztfQ-2ah8p7s0Vc19vTW3ruoyQ53JvjQjrw1zbuFVyO4SGVjdXtWkUQ-Nbz0h0SlIZLlafLbztm6-P4YXcdpdIpstKMIJMvICHzAtOrnZrpaiB6IRhC-MK~ntPjq0hM60U2OHfwHvC2Cgx7hKktHQVMucXOuu5czE5~XDa9ji0UCcp8zNOETns0JneQfkjKXQkoQBOxKE5DOqttkqSNYrdddhUi4eA__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+                src="/EQhsdAx9LYe1HLJgsF9i3GeTYnX7AXzA4nNpFFBcRaJt.jpg"
                 alt="Priya Sharma"
               />
             </a>
@@ -95,7 +95,7 @@ const Tipper = () => {
             <a href="#" className="w-full md:w-auto  lg:min-w-[300px]">
               <img
                 className="object-cover w-full h-full rounded-lg sm:rounded-none sm:rounded-l-lg"
-                src="https://cdn.gencraft.com/prod/user/e3b1d056-a391-4365-acd5-45c7d2f69279/6746e3b7-e720-4840-95e1-7a0713608e4d/image/image0_0.jpg?Expires=1725121477&Signature=WYzhuYvTylFl4v033DUZkGJCOD4qe791J3VPQnZw4TxQJbAZksCa5kOjVqiSxDaopPa9ClbyKbbRpuveF7sc8Vug3MsioMA0H8P3lie-NYx4JW-iZgI3cpS9BD0FQX20fzrTWcM3HpdScYsDCnk5GjJQtu8hLUV6p0WbIBwGRi5zxA7MWDJXdNx8beddHEzkNaV1KbyGbgWPTXglwI4z6b-TIXGaoL8c7Pre6Eol31pSxyfjfCyuPuLxU0I~2wmZLUjefcDe6xB5ndaoV6~aHgibxHq2jsp0hnFg~kCUaOqBBrwWmG83ZZEYv6fr3m2xut73UTC9A6xaElGmBasnqw__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+                src="/HA4y2LT19RJhQJfM8wRMNbiUzDC7iFtYf4M43ZjvqZYD.jpg"
                 alt="Neha Reddy"
               />
             </a>
@@ -133,7 +133,7 @@ const Tipper = () => {
             <a href="#" className="w-full md:w-auto  lg:min-w-[300px]">
               <img
                 className="object-cover w-full h-full rounded-lg sm:rounded-none sm:rounded-l-lg"
-                src="https://cdn.gencraft.com/prod/user/e3b1d056-a391-4365-acd5-45c7d2f69279/c1ce15c9-1a9d-4dce-9007-240d189e5a6f/image/image0_0.jpg?Expires=1725121402&Signature=bxRbyhcx8m3lQipDiJi7mX2MD6GRx0dCvd2m5ClARkbVJZxyNjuIyazhU7Wy3b28loBV24gKxrFsgEBBk4tnHhL6CZU425cYnjubayp7o2YE1IJykx5dKS79V1f2tAzdLGE-dS8pK6Q~5X8MBI-KE6F1BWCvcAynKlgWchkFkZP3wdmHrShOUPN0aFdIOhD38u9e-2m17OCvSgxwEpAea19S2gQIh3KRkg324jo0bGgrXgb4LYknmM2XTHHaCtN-iXa-2xQwo5sZcFS2ExHRKyprc~Wwzk526NrI51LT8BI0fk9B0-04gjBnYN2Ba0xa9ZxwxU6p5KHETFFypIgACg__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+                src="/2jCZccY7k2ZTBh8nb36rx7vviz4a68pwYJfFJzCTMqs5.jpg"
                 alt="Amit Patel"
               />
             </a>
@@ -142,8 +142,8 @@ const Tipper = () => {
                 <a href="#">Amit Patel</a>
               </h3>
               <p className="font-light text-gray-500 dark:text-gray-400 mb-4 lg:min-h-[180px]">
-                Amit is an experienced waiter at Swaad Restaurant, renowned for
-                his efficiency and courteous nature.
+                Amit is a hardworking server at Swaad Restaurant, well-known for
+                his efficiency and professional attitude.
               </p>
               <div className="flex gap-3">
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -158,7 +158,7 @@ const Tipper = () => {
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                   <Link
                     href={
-                      "https://dial.to/?action=solana-action%3Ahttps%3A%2F%2Ftipper-app.vercel.app%2Fapi%2Factions%2Ftransfer-send%3Fto%3D2%26amount%3D5&cluster=devnet"
+                      "https://dial.to/?action=solana-action%3Ahttps%3A%2F%2Ftipper-app.vercel.app%2Fapi%2Factions%2Ftransfer-send%3Fto%3D2%26amount%3D5&cluster=mainnet"
                     }
                   >
                     Tip $SEND
